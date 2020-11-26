7e99e901781895f88cb1cf9ef03f5396f575ee12
BhanukaC <50510335+BhanukaC@users.noreply.github.com>
2020-11-26 05:55:00 +0530

    Ir sensor

--- IR Sensor/IR Sensor/main.c
+++ IR Sensor/IR Sensor/main.c
@@ -3,21 +3,30 @@
 #endif
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+
 
 
 int main(void)
 {
 	DDRC=1<<PC4;
-	/* Replace with your application code */
+	
 	while (1)
 	{
-		if(PIND &(1<<PD6))//Check whether pc3 is high
+		
+		
+		
+		if(PIND &(1<<PD6))//Check whether PD6 is high
 		{
-			PORTC=PORTC | 1<<PC4;//output pc4 high
+			
+			PORTC=PORTC | (1<<PC4);//output pc4 high
+			
 		}
 		else{
 			PORTC=PORTC & ~(1<<PC4);////output pc4 low
 		}
+		
 		
 	}
 }
